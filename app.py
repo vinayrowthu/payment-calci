21a38801691dac54908f195333aad9c501a0921e
vinayrowthu <vinayrowthu@gmail.com>
2025-11-20 17:49:53 +0530

    Fix: Use Heroku PORT environment variable

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, send_from_directory
+from flask import Flask, request, jsonify, send_from_directory, os
 
 app = Flask(__name__)
 
@@ -18,4 +18,4 @@ def home():
     return send_from_directory('.', 'index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
